765ac0f07720c1c074b99892c0737b77233e14f2
lafius <duong.spthien@hotmail.fr>
2018-11-29 15:17:26 +0100

    ok

--- read.py
+++ read.py
@@ -1,6 +1,26 @@
 import csv
+import numpy as np
 
-with open('eggs.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+# parser node
+
+with open('nodesG.csv') as csvfile:
+    readcsv = csv.reader(csvfile, delimiter=';')
+    lineCount = -1
+    listeCoord = []
+    for row in readcsv:
+        lineCount = lineCount + 1
+        if(lineCount >= 1):
+            listeCoord.append(row)
+
+#parser distance
+
+with open('distancesG.csv') as csvfile:
+    readcsv = csv.reader(csvfile, delimiter=';')
+    matriceDistances = np.zeros((lineCount,lineCount))
+    listedist = []
+    for row in readcsv:
+        listedist.append(row[0])
+    print(listedist)
+    for i in range(lineCount):
+        for j in range(lineCount):
+            matriceDistances[i,j] = listedist[1+lineCount*i+j]
