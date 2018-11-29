728baea46af51f17c42d06014dc28f92f816525a
LouisRavillon <44974268+LouisRavillon@users.noreply.github.com>
2018-11-29 16:00:53 +0100

    Update read.py

--- read.py
+++ read.py
@@ -26,3 +26,17 @@ with open('distancesG.csv') as csvfile:
     for i in range(lineCount):
         for j in range(lineCount):
             matriceDistances[i,j] = listedist[lineCount*i+j]
+            
+
+def cout(b,c,matriceDistances):
+    cout_boucle = 0
+    cout_chaine = 0
+    for boucle in b:
+        for i in range(len(boucle)-1):
+            cout_boucle += matriceDistances[boucle[i],boucle[i+1]]
+        cout_boucle += matriceDistances[boucle[len(boucle)-1],boucle[0]]
+    for chaine in c:
+        for j in range(len(chaine)-1):
+            cout_chaine += matriceDistances[chaine[j],chaine[j+1]]
+    cout_reseau = cout_boucle + cout_chaine
+    return cout_reseau            
